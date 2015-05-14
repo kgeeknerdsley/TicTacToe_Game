37dcaf4854cbe44937d1b7addd5a3581d95c30e3
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-05-14 16:01:19 -0700

    Added some basic methods + game board array

--- GameCalcs.java
+++ GameCalcs.java
@@ -1,5 +1,17 @@
 package com.game.ttt;
 
 public class GameCalcs {
-
+	
+	public GameCalcs() {
+		String[][] gameboard = new String[3][3];
+	}
+	
+	public String boardDisplay() {
+		return "Testing one two three";
+	}
+	
+	
+	public void monitor() {
+		
+	}
 }
