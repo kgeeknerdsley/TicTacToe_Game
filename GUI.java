cce264aefa69d26a20720d72e97678897b98ba7e
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-05-14 16:00:33 -0700

    Added rudimentary board display code
    Display does not work but instead screws up the whole interface

--- GUI.java
+++ GUI.java
@@ -1,11 +1,15 @@
 package com.game.ttt;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,6 +25,9 @@ public class GUI extends JFrame implements ActionListener {
 	}
 		
 	private void initUI() {
+		JFrame mwindow = new JFrame();
+		
+		//buttons
 		JPanel panel = new JPanel();
 			
 		panel.setBorder(BorderFactory.createEmptyBorder(3,3,5,5));
@@ -36,12 +43,20 @@ public class GUI extends JFrame implements ActionListener {
 		
 		add(panel);
 		
-		setTitle("Tic Tac Toe Simulator 2k15");
-		setSize(500,500);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+		/*
+		 * THIS PART IS CURRENTLY BROKEN 
+		displays state of game board
+		JLabel boardDisplayer = new JLabel(ttt.boardDisplay());
+		mwindow.getContentPane().add(BorderLayout.EAST, boardDisplayer);
+		*/
 		
+		//master window setup
+		mwindow.setTitle("Tic Tac Toe Simulator 2k15");
+		mwindow.setSize(500,500);
+		mwindow.setLocationRelativeTo(null);
+		mwindow.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -57,7 +72,6 @@ public class GUI extends JFrame implements ActionListener {
 		System.out.println(event.paramString());
 		
 		if(event.getSource() == buttray[5]) { //tests for middle right, as example
-			System.out.println("It worked");
 		}
 	}	
 }
