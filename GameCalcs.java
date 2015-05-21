2d1a403e1607c345caa9f2ed0478878c6b0f3eb6
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-05-21 07:48:54 -0700

    Added switch case for space identification
    Create 2 methods, one for X and for O depending on turn

--- GameCalcs.java
+++ GameCalcs.java
@@ -4,17 +4,43 @@ import java.util.Arrays;
 
 public class GameCalcs {
 	String gameboard[][];
-	
+
 	public GameCalcs() {
 		gameboard = new String[3][3];
 	}
-	
+
 	public String boardDisplay() {
-		testFiller();
-		return Arrays.toString(gameboard);
+		return gameboard;
 	}
-	
-	
+
+	public String boardUpdateX(int identifier) {
+		String move = "X";
+
+		switch(identifier) {
+			case 0: gameboard[0][0] = move;
+					break;
+			case 1: gameboard[0][1] = move;
+					break;
+			case 2: gameboard[0][2] = move;
+					break;
+			case 3: gameboard[1][0] = move;
+					break;
+			case 4: gameboard[1][1] = move;
+					break;
+			case 5: gameboard[1][2] = move;
+					break;
+			case 6: gameboard[2][0] = move;
+					break;
+			case 7: gameboard[2][1] = move;
+					break;
+			case 8: gameboard[2][2] = move;
+					break;
+			case 9: gameboard[2][3] = move;
+					break;
+		}
+	}
+
+
 	public void testFiller() {
 		for(int i = 0; i < gameboard.length; i++) {
 			for(int j = 0; j < gameboard[0].length; j++) {
