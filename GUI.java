327dbb7369cd03fa94415215f9c3788dc9852fea
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-05-22 07:41:20 -0700

    Added rest of button registration commands

--- GUI.java
+++ GUI.java
@@ -7,39 +7,39 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class GUI extends JFrame implements ActionListener {
-	
+
 	GameCalcs ttt = new GameCalcs();
 	JButton[] buttray = new JButton[9];
 
 	public GUI() {
 		initGridUI();
 	}
-		
+
 	private void initGridUI() {
-		
+
 		//buttons
 		JPanel panel = new JPanel();
-			
+
 		panel.setBorder(BorderFactory.createEmptyBorder(3,3,5,5));
 		panel.setLayout(new GridLayout(3,3,10,10));
-			
+
 		String[] buttons = {"Top Left", "Top Middle", "Top Right", "Middle Left", "Middle", "Middle Right", "Bottom Left", "Bottom Middle", "Bottom Right"};
-		
+
 		for(int i = 0; i < buttons.length; i++) {
 			buttray[i] = new JButton(buttons[i]);
 			panel.add(buttray[i]);
 			buttray[i].addActionListener(this);
 		}
-		
+
 		add(panel);
-				
+
 		//master window setup
 		setTitle("Tic Tac Toe Simulator 2k15: The Buttons");
 		setSize(500,500);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
@@ -53,11 +53,43 @@ public class GUI extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		System.out.println(event.getActionCommand());
-		System.out.println(event.paramString());
-		
-		if(event.getSource() == buttray[5]) { //tests for middle right, as example
-			ttt.boardUpdateX(5);
+		//System.out.println(event.getActionCommand());
+		//System.out.println(event.paramString());
+
+		if(event.getSource() == buttray[0]) { //upper left
+			ttt.boardUpdate(0);
+		}
+
+		if(event.getSource() == buttray[1]) { //upper middle
+			ttt.boardUpdate(1);
+		}
+
+		if(event.getSource() == buttray[2]) { //upper right
+			ttt.boardUpdate(2);
+		}
+
+		if(event.getSource() == buttray[3]) { //middle left
+			ttt.boardUpdate(3);
+		}
+
+		if(event.getSource() == buttray[4]) { //middle middle
+			ttt.boardUpdate(4);
+		}
+
+		if(event.getSource() == buttray[5]) { //middle right
+			ttt.boardUpdate(5);
+		}
+
+		if(event.getSource() == buttray[6]) { //bottom left
+			ttt.boardUpdate(6);
+		}
+
+		if(event.getSource() == buttray[7]) { //bottom middle
+			ttt.boardUpdate(7);
+		}
+
+		if(event.getSource() == buttray[8]) { //bottom right
+			ttt.boardUpdate(8);
 		}
 	}
 
