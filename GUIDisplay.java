85aa2f66228a0bf3d05a9a670c0b7fe9cfd1b8e8
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-05-26 07:11:31 -0700

    Moved loc of board object
    Made accessible by all

--- GUIDisplay.java
+++ GUIDisplay.java
@@ -5,37 +5,37 @@ import java.awt.*;
 
 public class GUIDisplay extends JFrame {
 	GameCalcs ttt = new GameCalcs();
-	
+	JLabel board = new JLabel(ttt.boardDisplay());
+
 	public GUIDisplay() {
 		initUI();
 	}
-	
+
 	private void initUI() {
-		JLabel board = new JLabel(ttt.boardDisplay());
 		board.setForeground(new Color(50, 50 ,25));
-		
+
 		createLayout(board);
-		
+
 		setTitle("Tic Tac Toe Simulator 2k15: The Stats");
 		setSize(250,500);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
+
 	public void refreshWindow() {
 		SwingUtilities.updateComponentTreeUI(board);
 	}
-	
+
 	private void createLayout(JComponent... arg) {
 		Container pane = getContentPane();
 		GroupLayout gl = new GroupLayout(pane);
 		pane.setLayout(gl);
-		
+
 		gl.setAutoCreateContainerGaps(true);
-		
+
 		gl.setHorizontalGroup(gl.createSequentialGroup().addComponent(arg[0]));
 		gl.setVerticalGroup(gl.createSequentialGroup().addComponent(arg[0]));
-		
+
 		pack();
 	}
 }
