fa85d83a985298a2c066d5676074a310cc1ffeaf
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-05-27 07:45:38 -0700

    Created AI file, basic speak method

---
+++ AIBrain.java
@@ -0,0 +1,19 @@
+package com.game.ttt;
+
+import java.util.*;
+
+public class AIBrain extends TimerTask {
+
+	public AIBrain() {
+	}
+
+	public String speak() {
+		return "Testing one two three";
+	}
+
+	public void run() {
+		speak();
+	}
+
+
+}
