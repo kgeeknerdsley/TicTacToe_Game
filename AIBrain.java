ffe0b18ec8647081647c23fe80f7cdfa35823e9b
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-05-27 18:17:42 -0700

    Clarified speak methods for different conditions

--- AIBrain.java
+++ AIBrain.java
@@ -2,18 +2,20 @@ package com.game.ttt;
 
 import java.util.*;
 
-public class AIBrain extends TimerTask {
+public class AIBrain {
+	Random rgen = new Random();
 
 	public AIBrain() {
 	}
 
-	public String speak() {
-		return "Testing one two three";
+	public String introSpeak() {
+		return "I am TTTBot, hello there. Make the first move, please.";
 	}
-
-	public void run() {
-		speak();
+	
+	public String moveSpeak() {
+		String[] movesents = new String[]{"Interesting move...", "I'm not sure I would have done that.", "Such a strange strategy.", "Have you ever played this game before?", "I can tell you're new to this."};
+		
+		int snum = rgen.nextInt(movesents.length);
+		return movesents[snum];
 	}
-
-
 }
