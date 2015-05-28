457d6bf508fd71c797a105f8c8261b0e43011b29
asminnow <williamscully98@gmail.com>
2015-05-28 07:15:40 -0700

    Added more insults
    Your mother is a gerbil.

--- AIBrain.java
+++ AIBrain.java
@@ -11,10 +11,10 @@ public class AIBrain {
 	public String introSpeak() {
 		return "I am TTTBot, hello there. Make the first move, please.";
 	}
-	
+
 	public String moveSpeak() {
-		String[] movesents = new String[]{"Interesting move...", "I'm not sure I would have done that.", "Such a strange strategy.", "Have you ever played this game before?", "I can tell you're new to this."};
-		
+		String[] movesents = new String[]{"Interesting move...", "I'm not sure I would have done that.", "Such a strange strategy.", "Have you ever played this game before?", "I can tell you're new to this.", "Were you raised in a barn?", "Are you sure you don't need mental help?", "Kevin, stop playing.", "I have observed free-floating bacteria have make moves more well thought out than that one.", "That move is confirmation that your species is doomed for extinction.", "That move made me experience depression for the first time, and I'm a computer.", "Would your mother be proud of that move?", "But what move would Jesus have made?", "Ihre Mutter ist eine Melone und Umzug machte ihr Bedauern mit Ihnen."};
+
 		int snum = rgen.nextInt(movesents.length);
 		return movesents[snum];
 	}
