4f05cdec6f98ea4739eee6bc86fe9476a8b1ffae
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-06-02 07:49:26 -0700

    Added new methods to save space in GUI

--- Button.java
+++ Button.java
@@ -1,24 +1,46 @@
 package com.game.ttt;
 
-import javax.swing.JButton;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
 
 public class Button extends JButton {
+	GameCalcs calc = new GameCalcs();
+	DialogBoxes dialog = new DialogBoxes();
+
 	boolean isPressed;
-	
-	public Button() {
+	int ident;
+
+	public Button(int identifier) {
 		super();
 		isPressed = false;
+		ident = identifier;
 	}
-	
+
 	public boolean isPressed() {
 		return isPressed;
 	}
-	
+
 	public void setPress(boolean state) {
 		isPressed = state;
 	}
-	
+
 	public void setX() {
 		setText("X");
+	}
+
+	public void setO() {
+		setText("O");
+	}
+
+	public void pressedX() {
+		calc.boardUpdateX(ident);
+		setX();
+	}
+
+	public void pressedO() {
+		calc.boardUpdateO(ident);
+		setO();
 	}
 }
