7a4dbe15ff046ff1b1cffcb5d3f655ab4818a8c5
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-06-04 07:38:14 -0700

    Temporarily edited methods to remove GameCalcs dependency until workaround found

--- Button.java
+++ Button.java
@@ -6,8 +6,7 @@ import javax.swing.*;
 import java.util.*;
 
 public class Button extends JButton {
-	GameCalcs ttt = new GameCalcs();
-
+	//GameCalcs ttt = new GameCalcs();
 
 	boolean isPressed;
 	int ident;
@@ -35,13 +34,15 @@ public class Button extends JButton {
 	}
 
 	public void pressedX(int bnum) {
-		ttt.boardUpdateX(bnum);
+		//ttt.boardUpdateX(bnum);
 		setX();
+		//ttt.boardPrinter();
 		setPress(true);
 	}
 
 	public void pressedO(int bnum) {
-		ttt.boardUpdateO(bnum);
+		//ttt.boardUpdateO(bnum);
+		//ttt.boardPrinter();
 		setO();
 	}
 }
