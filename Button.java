9d9ff0d6e59cfa72a9d3d13fe9cdca43d23c266d
Gavin Worsley <kgeeknerdsley@users.noreply.github.com>
2015-06-09 07:35:47 -0700

    Added icons to buttons

--- Button.java
+++ Button.java
@@ -1,18 +1,30 @@
 package com.game.ttt;
 
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 import javax.swing.*;
 
 public class Button extends JButton {
 	GameCalcs calc = new GameCalcs();
 
-
 	boolean isPressed;
 	int ident;
+
+	BufferedImage xTemp = null;
+	BufferedImage oTemp = null;
+
+	ImageIcon xIcon;
+	ImageIcon oIcon;
 
 	public Button(int identifier) {
 		super();
 		isPressed = false;
 		ident = identifier;
+		loadXIcon();
+		loadOIcon();
 	}
 
 	public boolean isPressed() {
@@ -23,14 +35,36 @@ public class Button extends JButton {
 		isPressed = state;
 	}
 
+	public void loadXIcon() {
+		try {
+			xTemp = ImageIO.read(new File("X.png"));
+		} catch (IOException e) {
+			System.out.println("Error loading the X icon.");
+		}
+
+		xIcon = new ImageIcon(xTemp);
+	}
+
+	public void loadOIcon() {
+		try {
+			oTemp = ImageIO.read(new File("O.png"));
+		} catch (IOException e) {
+			System.out.println("Error loading the O icon.");
+		}
+
+		oIcon = new ImageIcon(oTemp);
+	}
+
 	public void setX() {
 		setText("X");
 		setPress(true);
+		setIcon(xIcon);
 	}
 
 	public void setO() {
 		setText("O");
 		setPress(true);
+		setIcon(oIcon);
 	}
 
 	public int getID() {
